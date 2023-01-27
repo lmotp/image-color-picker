2efb2a68c0bf969a1ad5051fa769f994874890c5
lmotp <lmotp@daum.net>
2023-01-28 00:36:13 +0900

    feat : image file upload and getImageData rgb

--- src/pages/ColorPicker/index.tsx
+++ src/pages/ColorPicker/index.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import './styles.css';
 
 function ColorPicker() {
   const imgFileRef = useRef(null);
@@ -6,25 +7,50 @@ function ColorPicker() {
 
   const onClickForm = () => {
     const imgFile = imgFileRef.current;
-    const image = new Image();
+    const image: HTMLImageElement = new Image();
     const file = imgFile.files[0];
     const fileReader = new FileReader();
 
     fileReader.onload = () => {
       image.onload = () => {
         const canvas = canvasRef.current;
-        canvas.width = image.width;
-        canvas.height = image.height;
+        canvas.width = image.width / 2;
+        canvas.height = image.height / 2;
         const ctx = canvas.getContext('2d');
-        ctx.drawImage(image, 0, 0);
+        ctx.drawImage(image, 0, 0, image.width / 2, image.height / 2);
 
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        console.log(imageData);
+
+        const rgbArray = buildRgb(imageData.data);
       };
+
+      image.src = URL.createObjectURL(file);
     };
+
+    fileReader.readAsDataURL(file);
+  };
+
+  const buildRgb = (imageData: Uint8ClampedArray) => {
+    const rgbValues = [];
+    // note that we are loopin every 4!
+    // for every Red, Green, Blue and Alpha
+    for (let i = 0; i < imageData.length; i += 4) {
+      const rgb = {
+        r: imageData[i],
+        g: imageData[i + 1],
+        b: imageData[i + 2],
+      };
+
+      rgbValues.push(rgb);
+    }
+
+    return rgbValues;
   };
 
   return (
-    <>
+    <section>
       <form onSubmit={(e) => e.preventDefault()}>
         <input type="file" id="imgfile" ref={imgFileRef} />
         <input type="button" id="btnLoad" value="Load" onClick={onClickForm} />
@@ -32,7 +58,7 @@ function ColorPicker() {
       <canvas id="canvas" ref={canvasRef}></canvas>
       <div id="palette"></div>
       <div id="complementary"></div>
-    </>
+    </section>
   );
 }
 
